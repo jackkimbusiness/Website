e7c2af8abd519c5d4d81a0e327a9155d672b9893
Jack Kim <41196859+jackkimbusiness@users.noreply.github.com>
2019-01-29 14:18:25 -0500

    Update Home.js

--- JackKim.com/JSfiles/Home.js
+++ JackKim.com/JSfiles/Home.js
@@ -1,2 +1,18 @@
-var title = "Jack"
-console.log(title)
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var navbar = document.getElementByID("navbar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+/* Add the sticky class to the navbar when you reach its scroll position. 
+Remove the "sticky" when you leave the scroll position.*/
+function myFunction() {
+	if (window.pageYOffset >= sticky) {
+		navbar.classList.add("sticky")
+	} else {
+		navbar.classList.remove("sticky");
+	}
+}
